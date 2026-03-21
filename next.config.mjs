f864771c41baf1b2ec9d11ea2e5352dcb00c4634
nishi147 <nsk110761@gmail.com>
2026-03-21 13:12:14 +0530

    server error fixeddd

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://backend-olive-five-70.vercel.app/api/:path*',
       },
     ];
   },
